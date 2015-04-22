fc7c5c6e30ea676980a862a1db6bc51f6d63fc00
Rafael Giusti <rfgiusti@gmail.com>
2015-04-22 09:47:52 -0300

    tb.version returns a struct

--- +tb/checkversion.m
+++ +tb/checkversion.m
@@ -4,52 +4,63 @@ function checkversion(version)
 %   CHECKVERSION('1.0') will throw an exception if the current TimeBox
 %   version is older than '1.0'.
 %
+%   CHECKVERSION(struct('major', 1, 'minor', 0)) is the same as the above.
+%
+%   CHECKVERSION(TB.VERSION) will always run silently.
+%
+%   CHECKVERSION('1.0.5') is the same as CHECKVERSION('1.0'). The extra
+%   digit is treated as the release number, which is ignored.
+%
 %   CHECKVERSION('1.0+stable') will throw an exception if the current
-%   TimeBox version is older than '1.0' or if the current TimeBox
-%   version is specified as unstable.
+%   TimeBox version is older than 1.0 or if the current TimeBox version
+%   does not have the flag 'stable'.
+%
+%   CHECKVERSION('1.0-stable') will throw an exception if the current
+%   TimeBox version is older than 1.0 or if the current TimeBox has the
+%   flag 'stable'.
 %
 %   If the current version is not backwards-compatible with the expected
 %   version, an exception will be thrown.
-current = breakversion(tb.version);
-specified = breakversion(version);
-if versioncmp(current, specified) < 0
-    error(['Required TimeBox version ' version ' or later (currently running TimeBox version ' tb.version ')']);
+if isequal(class(version), 'char')
+    specifiedversion = string2version(version);
+else
+    specifiedversion = version;
 end
 
-if current.stable == -1 && specified.stable == 1
-    error(['You are currently running an alpha (unstable) version of TimeBox. Please get stable version %d.%d or ' ...
-        'compatible at http://github.com/rfgiusti/timebox'], specified.major, specified.minor);
+if versioncmp(tb.version, specifiedversion) < 0
+    error(['Required TimeBox version ' version ' or later (currently running TimeBox version ' ...
+        version2string(tb.version) ')']);
 end
 
-% If there is ever a backwards-compatibility break in TimeBox, uncomment
-% the verification below
-% % Version x.y breaks compatibility of TS.LOAD. If TimeBox was expected
-% % to be of version 0.x or inferior, raises a non-backwards compatibility
-% % exception
+% % If there is ever a backwards-compatibility break in TimeBox, uncomment
+% % the verification below
+% % Version x.y breaks compatibility of XX.YYY. If TimeBox was expected
+% % to be of any version prior to x.y, then it was expected to provide
+% % funcionalities that are potentially not supported. An exception should be
+% % raised.
 % breaksafter = struct('major', x, 'minor', y);
-% if versioncmp(specified, breaksafter) < 0
-%     error(['The current TimeBox version ' tb.version ' is not compatible with the expected version ' version '. ' ...
-%         'Backwards-compatibility was broken in version %d.%d. Please visit http://github.com/rfgiusti/timebox for ' ...
-%         'an older version of TimeBox.', breaksafter.major, breaksafter.minor]);
+% if versioncmp(specifiedversion, breaksafter) < 0
+%     error(['The current TimeBox version ' version2string(tb.version) ' is not compatible with the expected version ' ...
+%         version '. Backwards-compatibility was broken in TimeBox %d.%d. Please visit ' ...
+%         'http://github.com/rfgiusti/timebox for an older version of TimeBox.'], breaksafter.major, breaksafter.minor);
 % end
 end
 
 
-function vstruct = breakversion(vstring)
-tmpcell = regexp(vstring, '^(\d+)\.(\d+)(+stable|+unstable)?$', 'tokens');
-tokens = tmpcell{1};
+function vstruct = string2version(vstring)
+S = regexp(vstring, '^(\d+)\.(\d+)', 'tokens');
+tokens = S{1};
+if numel(tokens) ~= 2
+    error('tb:checkversion', ['Version string "' vstring '" is invalid']);
+end
 major = sscanf(tokens{1}, '%d');
 minor = sscanf(tokens{2}, '%d');
-if isempty(tokens{3})
-    stable = 0;
-elseif isequal(tokens{3}, '+stable')
-    stable = 1;
-elseif isequal(tokens{3}, '+unstable')
-    stable = -1;
-else
-    error(['Version string "' vstring '" is invalid']);
+vstruct = struct('major', major, 'minor', minor);
 end
-vstruct = struct('major', major, 'minor', minor, 'stable', stable);
+
+
+function vstring = version2string(vstruct)
+vstring = sprintf('%d.%d', vstruct.major, vstruct.minor);
 end
 
 
--- +tb/version.m
+++ +tb/version.m
@@ -1,4 +1,8 @@
 function ver = version
 %TB.VERSION     Return the version of TimeBox
-ver = '0.3';
+ver = struct;
+ver.major = 0;          % major version; increase with big changes. Currently unreleased
+ver.minor = 5;          % minor version; increase with new bug fixes "small" new functionalities
+ver.release = 0;        % release number; always increases. Currently unreleased
+ver.special = '';       % set flags for particular versions
 end
