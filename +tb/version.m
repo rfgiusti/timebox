18382c4e1ca4f6cb22fa91cd3b73b29bd3bd8e14
Rafael Giusti <rfgiusti@gmail.com>
2015-04-27 12:26:44 -0300

    Add license; perform very first release

--- +tb/version.m
+++ +tb/version.m
@@ -1,8 +1,9 @@
 function ver = version
 %TB.VERSION     Return the version of TimeBox
 ver = struct;
-ver.major = 0;          % major version; increase with big changes. Currently unreleased
-ver.minor = 5;          % minor version; increase with new bug fixes "small" new functionalities
-ver.release = 0;        % release number; always increases. Currently unreleased
-ver.special = '';       % set flags for particular versions
+ver.major = 0;                    % major version; increase with big changes. Currently unreleased
+ver.minor = 6;                    % minor version; increase with new bug fixes "small" new functionalities
+ver.release = 1;                  % release number; always increases. Currently unreleased
+ver.releasedate = 'Apr-27-2015';  % MM-DD-YYYY date of last stable release
+ver.special = '';                 % set flags for particular versions
 end
--- README.md
+++ README.md
@@ -7,8 +7,13 @@ Purpose
 TimeBox is a procedural library that implements data structures and functions for the evaluation
 of time series classification methods.
 
-This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
-even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in
+compliance with the License. You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+This software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+either express or implied.
 
 
 Installing TimeBox
