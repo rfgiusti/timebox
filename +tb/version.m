c6c97080fd98f1dc043460d7077d6d2d59404682
Rafael Giusti <rfgiusti@gmail.com>
2015-09-08 15:00:54 -0300

    TB.VERSION internal documentation

--- +tb/version.m
+++ +tb/version.m
@@ -9,7 +9,8 @@ ver = struct;
 ver.major = 0;                               % major version; increase with big changes. Currently unreleased
 ver.minor = 8;                               % minor version; increase with new bug fixes "small" new functionalities
 ver.release = 2;                             % release number; always increases
-ver.releasedate = 'May-19-2015';             % MM-DD-YYYY date of last stable release
-ver.releasedatenum = datenum(2015, 05, 19);  % date number value for the date of last stable release
-ver.special = 'alpha unreleased';            % set flags for particular versions
+ver.releasedate = 'May-19-2015';             % MM-DD-YYYY date of last stable release; present before 0.8.3 and kept for
+                                             % backwards compatibility
+ver.releasedatenum = datenum(2015, 05, 19);  % date number value for the date of last stable release; added in 0.8.3
+ver.special = 'alpha+unreleased';            % set flags for particular versions
 end
