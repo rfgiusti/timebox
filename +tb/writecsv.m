813ee1e2106f3f7d1d4a8dc421076f11611c1d75
Rafael Giusti <rfgiusti@gmail.com>
2015-11-24 14:14:52 -0200

    Added optional separator argument for TB.WRITECSV
    - The function may be called as WRITECSV(p,sep,H,...) if "sep" is of
      type CHAR
    - Default separator is now the tabulator character (\t)

--- +tb/writecsv.m
+++ +tb/writecsv.m
@@ -2,7 +2,9 @@ function writecsv(savepath, header, varargin)
 %TB.WRITECSV    Write a comma seaprated value file
 %   WRITECSV(p,H,D) writes to the file in `p' the contents of the cell
 %   array D, using H as the headers. D must be a n-by-numel(H) cell
-%   where each element is either CHAR or DOUBLE.
+%   where each element is either CHAR or assumed DOUBLE. If the cell
+%   contains a value that can't be cast to a DOUBLE (i.e, a CELL), the
+%   behavior is undefined.
 %
 %   WRITECSV(p,H,D1,D2,...,Dm) does the same, but considers that the
 %   data to be saved is spread across the cell arrays D1,D2,...,Dm. Each
@@ -16,8 +18,34 @@ function writecsv(savepath, header, varargin)
 %       d2 = {'c1'; 'c2'; 'c3'};
 %       tb.writecsv('/tmp/test.csv', {'a','b','c'}, d1, d2);
 %
+%   WRITECSV(p,sep,H,...), when sep is of class CHAR, works as either
+%   previous versions, but sep is taken as the separator. By default,
+%   the separator is the tab character. The separator must be such that
+%   it is a valid FORMAT argument for the FPRINTF function. No
+%   verifications are performed. If the separator contains a character
+%   that would cause FPRINTF to read additional arguments, the behavior
+%   is undefined.
+%
+%       Example:
+%
+%       d1 = {'a1' 'b1'; 'a2' 'b2'; 'a3' 'b3'};
+%       d2 = {'c1'; 'c2'; 'c3'};
+%       % INVALID: tb.writecsv('/tmp/test.csv', '%', {'a','b','c'}, d1, d2);
+%       tb.writecsv('/tmp/test.csv', ';', {'a','b','c'}, d1, d2);
+if isequal(class(header), 'char')
+    separator = header;
+    header = varargin{1};
+    firstarg = 2;
+    tb.assert(numel(separator) == 1 && separator ~= '\' || numel(separator) == 2 && separator(1) == '\', ...
+        ['Invalid seaprator: "' separator '"']);
+else
+    separator = '\t';
+    firstarg = 1;
+end
+tb.assert(numel(varargin) >= firstarg, 'Some data must be supplied');
+
 numcols = 0;
-for i = 1:numel(varargin)
+for i = firstarg:numel(varargin)
     numcols = numcols + size(varargin{i}, 2);
 end
 tb.assert(numcols == numel(header), 'Total number of columns does not match number of elements in the header');
@@ -32,16 +60,16 @@ tb.assert(f > 0, ['Error opening file ''' savepath ''' for write']);
 
 fprintf(f, '%s', header{1});
 for i = 2:numel(header)
-    fprintf(f, ',%s', header{i});
+    fprintf(f, [separator '%s'], header{i});
 end
 fprintf(f, '\n');
 
 for row = 1:size(varargin{1}, 1)
     first = 1;
-    for arg = 1:numel(varargin)
+    for arg = firstarg:numel(varargin)
         for i = 1:size(varargin{arg}, 2)
             if ~first
-                fprintf(f, ',');
+                fprintf(f, separator);
             end
             
             value = varargin{arg}{row,i};
