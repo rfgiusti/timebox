01eeb23f38e9b2a5c64189ea61efad3411d3235d
Rafael Giusti <rfgiusti@gmail.com>
2015-11-30 10:36:26 -0200

    Fix issue #6 for RUNS.DME.MERGE
    - The function now takes a list of class labels instead of the number of
      labels.

--- +runs/+dme/merge.m
+++ +runs/+dme/merge.m
@@ -1,4 +1,4 @@
-function [acc, classes] = merge(votes, weights, rankings, testclasses, numclasses)
+function [acc, classes] = merge(votes, weights, rankings, testclasses, labels)
 %RUNS.DME.MERGE     Evalutes the ensemble output and determines the class
 %assigned by the ensemble to each test instance
 %   This function is part of the ensemble evaluation set.
@@ -11,25 +11,27 @@ function [acc, classes] = merge(votes, weights, rankings, testclasses, numclasse
 %   The first 3 arguments of MERGE are the same returned by all ensemble
 %   functions in RUNS.DME.
 %
-%   MERGE(V,W,[],T,n) decides for class labeling ensembles, where V and W
-%   are the votes and the weight matrices as returned by labeling ensemble
-%   functions of RUNS.DME. The third argument may be an empty matrix or an
-%   empty cell. The arguments T and n stand for the training classes and
-%   number of classes. The output of MERGE is the accuracy of the ensemble
-%   on the test data set.
+%   MERGE(V,W,[],T,L) decides for class labeling ensembles, where V and
+%   W are the votes and the weight matrices as returned by labeling
+%   ensemble functions of RUNS.DME. The third argument may be an empty
+%   matrix or an empty cell. The arguments T and L stand for the
+%   training classes assigned with each test instance and the valid
+%   label set. The output of MERGE is the accuracy of the ensemble on
+%   the test data set. 
 %
 %   Usage example:
 %
 %       % The arguments of RUNS.DME.MAJORITY are ommitted for brevity.
 %       % Please refer to the ensemble documentation for details.
-%       numclasses = %number of classes for the data set
-%       numinstances = %number of test instances in the data set
+%       testclasses = %the classes of each test instance
+%       labels = %the valid labels for this classification probelm
 %       [votes, weights, rankings] = runs.dme.majority(...);
-%       merge(votes, weights, rankings, numclasses, numinstances);
+%       merge(votes, weights, rankings, testclasses, labels)
 %
-%   MERGE([],[],R,c,n) decides for label ranking ensembles, where the votes
-%   and weight matrices are empty and R is the ranking cell as returned by
-%   the label ranking ensembles.
+%   MERGE([],[],R,T,L) decides for label ranking ensembles, where the
+%   votes and weight matrices are empty and R is the ranking cell as
+%   returned by the label ranking ensembles. The order of the labels in
+%   L must be the same as used to produce the rankings.
 %
 %   [a,C] = MERGE(...) works as the two forms above, but also returns the
 %   class labels assigned to each test instance in addition to the
@@ -37,6 +39,9 @@ function [acc, classes] = merge(votes, weights, rankings, testclasses, numclasse
 %
 %   For more information on how ensemble evaluation is implemented in
 %   TimeBox, please check RUNS.DME.MAJORITY.
+numlabels = numel(labels);
+numinstances = numel(testclasses);
+
 if isempty(votes)
     % Label ranking. The weight matrix must also be empty and the ranking
     % cell cannot be empty.
@@ -45,7 +50,7 @@ if isempty(votes)
     tb.assert(~isempty(rankings), ['Vote matrix is empty, therefore label ranking ensemble was assumed, ' ...
         'in which case the ranking cell must NOT be empty.']);
     
-    votesperclass = meanranks(rankings, numclasses, numel(testclasses));
+    votesperclass = meanranks(rankings, numlabels, numinstances);
 else
     % Label assigner. The weight matrix must not be empty and the ranking
     % cell must be empty.
@@ -54,7 +59,7 @@ else
     tb.assert(isempty(rankings), ['Vote matrix is NOT empty, therefore CLASS labeling ensemble was assumed, ' ...
         'in which case the ranking cell MUST be empty.']);        
     
-    votesperclass = meanvotes(votes, weights, numclasses, numel(testclasses));
+    votesperclass = meanvotes(votes, weights, labels, numlabels, numinstances);
 end
 classes = zeros(numel(testclasses), 1);
 for i = 1:numel(testclasses)
@@ -64,9 +69,9 @@ for i = 1:numel(testclasses)
     % among them
     mostvoted = find(instancevotes == max(instancevotes));
     if numel(mostvoted) > 1
-        classes(i) = randsample(mostvoted, 1);
+        classes(i) = labels(randsample(mostvoted, 1));
     else
-        classes(i) = mostvoted;
+        classes(i) = labels(mostvoted);
     end
 end        
 
@@ -74,18 +79,18 @@ acc = mean(classes == testclasses);
 end
 
 
-function weightedvotes = meanvotes(votes, weights, numclasses, numinstances)
-    weightedvotes = zeros(numclasses, numinstances);
-    for class = 1:numclasses
-        weightedvotes(class, :) = sum(weights .* (votes == class), 1);
+function weightedvotes = meanvotes(votes, weights, labels, numlabels, numinstances)
+    weightedvotes = zeros(numlabels, numinstances);
+    for c = 1:numlabels
+        weightedvotes(c, :) = sum(weights .* (votes == labels(c)), 1);
     end
 end
 
 
-function sumofranks = meanranks(rankings, numclasses, numinstances)
-    sumofranks = zeros(numclasses, numinstances);
-    for c = 1:numel(rankings)
-        sumofranks = sumofranks + rankings{c};
+function sumofranks = meanranks(rankings, numlabels, numinstances)
+    sumofranks = zeros(numlabels, numinstances);
+    for i = 1:numel(rankings)
+        sumofranks = sumofranks + rankings{i};
     end
     
     % transform rank into points
