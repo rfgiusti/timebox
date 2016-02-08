6d5c4bfb511281e6f97683e395e25315aba743ea
Rafael Giusti <rfgiusti@gmail.com>
2016-02-08 15:09:21 -0200

    Add option to specify index in TS.MAKEFOLDS

--- +ts/makefolds.m
+++ +ts/makefolds.m
@@ -1,4 +1,4 @@
-function [test_folds, train_folds] = makefolds(ds, nfolds)
+function [test_folds, train_folds] = makefolds(ds, nfolds, index)
 %TS.MAKEFOLDS   Cross-validation indices.
 %   [X,Y] = MAKEFOLDS(DS) creates random folds for 10-fold
 %   cross-validation of the objects in data set DS. Each output value, X
@@ -12,28 +12,64 @@ function [test_folds, train_folds] = makefolds(ds, nfolds)
 %
 %   Example:
 %       train = ts.load('some data set');
-%       [trainfolds, testfolds] = ts.makefolds(train);
+%       [testfolds, trainfolds] = ts.makefolds(train);
 %       fold1train = train(trainfolds{1});
 %       fold1test = train(testfolds{1});
 %       runs.partitioned(fold1train, fold1test)
 %
+%   MAKEFOLD uses only the first row of DS, therefore it is possible to
+%   create indices with only the instance labels. In that case DS must
+%   be a row vector, otherwise it will be treated as a data set with a
+%   single instance.
+%
 %   [X,Y] = MAKEFOLDS(DS,k) returns indices for k-fold cross-validation,
 %   where k must be a positive integer.
 %
-%   Limitations: classes must be sequential positive integers.
-if ~exist('nfolds', 'var')
+%   [X,Y] = MAKEFOLDS(DS,[],I)
+%   [X,Y] = MAKEFOLDS(DS,k,I) makes indices for cross-validation on the
+%   instances of DS listed in I. This may be used to further partition
+%   an already partitioned data set -- e.g., it may be used to create
+%   validation sets. The indices in X and Y are still given with respect
+%   to the original data set DS.
+%
+%   Example:
+%       train = ts.load('some data set');
+%       [X,Y] = ts.makefolds(train, 2);
+%
+%       % Make 2 folds to validate the training data of the first fold
+%       [valX,valY] = ts.makefolds(train, 2, Y{1});
+%
+%       % Validate first fold
+%       acc = zeros(10,1);
+%       for i = 1:2
+%           validation1train = train(valY{i},:);
+%           validation1test = test(valX{i},:);
+%           acc(i) = runs.partitioned(validation1train, validation1test);
+%       end
+%       mean(acc)
+if ~exist('nfolds', 'var') || isempty(nfolds)
     nfolds = 10;
 end
-nseries = size(ds, 1);
+
+if exist('index', 'var')
+    tb.assert(isequal(class(index),'double') && size(index,1) == 1, '''index'' must be a column vector of double');
+    tb.assert(min(index) >= 1, 'Invalid index value: %d', min(index));
+    tb.assert(max(index) <= size(ds,1), 'Invalid index value: %d', max(index));
+    nseries = numel(index);    
+else
+    nseries = size(ds, 1);
+    index = 1:nseries;
+end
 
 if nseries < nfolds
     err = MException('InputChk:NotEnoughSamples', 'Input data set too small');
     throw(err);
 end
 
+
 % Make index of class -> instance number
-classes = ds(:, 1);
-class_index = [classes, (1:nseries)'];
+classes = ds(index, 1);
+class_index = [classes, index'];
 
 % Make empty folds
 test_folds = cell(nfolds, 1);
