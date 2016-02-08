8a92c0472ba4aceb0447a545fa8846a41fe635f2
Rafael Giusti <rfgiusti@gmail.com>
2016-02-08 15:34:39 -0200

    TS.GETPATH: return directory along with file path

--- +ts/getpath.m
+++ +ts/getpath.m
@@ -1,4 +1,4 @@
-function filepath = getpath(dsname, representation)
+function [filepath, filedir] = getpath(dsname, representation)
 %TS.GETPATH     Return the expected path of a data set in the local
 %repository.
 %   GETPATH(ds) returns the path where the data set "ds" should be
@@ -12,9 +12,16 @@ function filepath = getpath(dsname, representation)
 %   Example:
 %       >> ts.getpath('Coffee', 'time')
 %       >> ts.getpath('Coffee', 'ps')
+%
+%   [f,d] = GETPATH(ds)
+%   [f,d] = GETPATH(ds, rep) returns the path to the data set file in
+%   "f" and the directory where "f" is stored in "d". The last character
+%   of "d" is a slash ('/').
 if exist('representation', 'var') && ~isequal(representation, lower('time'))
     filepath = [tb.getdspath dsname '/transforms/' dsname '-' representation '.mat'];
+    filedir = [tb.getdspath dsname '/transforms/'];
 else
     filepath = [tb.getdspath dsname '/' dsname '.mat'];
+    filedir = [tb.getdspath dsname '/'];
 end
 end
