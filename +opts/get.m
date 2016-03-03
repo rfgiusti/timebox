ce0ff6cdffeb02a32565e2f3c9fca8614d830414
Rafael Giusti <rfgiusti@gmail.com>
2016-03-03 18:58:23 -0300

    Fix documentation of most OPTS functions

--- +opts/get.m
+++ +opts/get.m
@@ -1,4 +1,12 @@
 function value = get(set, name, default)
+%OPTS.GET   Return a value associated with a key in the OPTS object, or a
+%default value if no key exists.
+%   OPTS.GET(options,k) returns the value associated with they key "k" in
+%   the OPTS object "options". If the key is not present in the OPTS
+%   object, returns zero.
+%
+%   OPTS.GET(options,K,default) does the same, but returns "default" if the
+%   key is not present in the OPTS object.
 if ~exist('default', 'var')
     default = 0;
 end
