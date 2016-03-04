c126a7cb9c377f9f5109fe09c7964ab94e321a5e
Rafael Giusti <rfgiusti@gmail.com>
2016-03-04 14:13:48 -0300

    TimeBox 0.10.6
    - Introduces functions to get information on data sets without the need
      to load the full data: TS.LOADMETA and TS.SAVEMETA handle meta-
      information within data sets, and TS.GETDIM retrieves the number of
      instances and the length of the series.
    - Introduces TS.GETPATH to return the path where a data set file is
      supposed to be stored.
    - Allows TS.MAKEFOLDS to make validation folds: you may provide an index
      over an existing fold to obtain a new set of folds constructed on the
      indexed data.
    - Adds TB.SHARPSHOOT to produce SharpShoot plots, and add option
      -nofigure to TB.SCAT. This is useful to use TB.SCAT in subplots.
    - Adds TB.MERGECELLS to work with data loaded by TS.LOADFILES: this
      function actually concatenates cells with equal number of rows into a
      single cell, removing rows where any of the original cell had missing
      data.
    - Fixes bugs with TB.SPLITSTRING, TS.GETNAMES, and TS.SAVE. Also
      improves documentation of several functions and some error messages
      are now more informative.

--- +tb/version.m
+++ +tb/version.m
@@ -2,17 +2,17 @@ function ver = version
 %TB.VERSION     Return the version of TimeBox
 %   Release dates:
 %
-%   0.10.6 - Published; unreleased
+%   0.10.6 - Mar-04-2016
 %   0.9.5  - Nov-30-2015
 %   0.8.4  - Sep-14-2015
 %   0.8.3  - Sep-09-2015
 %   0.7.2  - May-19-2015
 %   0.6.1  - Apr-27-2015
 ver = struct;
-ver.major = 0;                               % major version; increase with big changes. Currently unreleased
-ver.minor = 10;                              % minor version; increase with new bug fixes "small" new functionalities
-ver.release = 5;                             % release number; always increases
-ver.releasedate = 'Nov-30-2015';             % MM-DD-YYYY date of last stable release; DEPRECTATE SINCE 0.8.4
-ver.releasedatenum = datenum(2015, 11, 30);  % date number value for the date of release
-ver.special = 'unstable';                    % set flags for particular versions
+ver.major = 0;                               % major version: increase with "big" changes
+ver.minor = 10;                              % minor version: increase with bug fixes and "small" new functionalities
+ver.release = 6;                             % release number: increase at every release
+ver.releasedate = 'Mar-04-2016';             % MM-DD-YYYY date of last stable release; DEPRECTATED SINCE v0.8.4
+ver.releasedatenum = datenum(2016, 03, 04);  % date number value for the date of release
+ver.special = '';                            % set flags for particular versions
 end
