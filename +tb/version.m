9efd5d1edbfc4e8873afb2682dffb0eb878b5839
Rafael Giusti <rfgiusti@gmail.com>
2016-03-04 20:04:02 -0300

    Start new development branch

--- +tb/version.m
+++ +tb/version.m
@@ -2,6 +2,7 @@ function ver = version
 %TB.VERSION     Return the version of TimeBox
 %   Release dates:
 %
+%   0.11.7 - predicted
 %   0.10.6 - Mar-04-2016
 %   0.9.5  - Nov-30-2015
 %   0.8.4  - Sep-14-2015
@@ -10,7 +11,7 @@ function ver = version
 %   0.6.1  - Apr-27-2015
 ver = struct;
 ver.major = 0;                               % major version: increase with "big" changes
-ver.minor = 10;                              % minor version: increase with bug fixes and "small" new functionalities
+ver.minor = 11;                              % minor version: increase with bug fixes and "small" new functionalities
 ver.release = 6;                             % release number: increase at every release
 ver.releasedate = 'Mar-04-2016';             % MM-DD-YYYY date of last stable release; DEPRECTATED SINCE v0.8.4
 ver.releasedatenum = datenum(2016, 03, 04);  % date number value for the date of release
