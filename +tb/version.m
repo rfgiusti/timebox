b0a6d3d84e8df66feb69d5f2c9d1ca7ed71bd8d2
Rafael Giusti <rfgiusti@gmail.com>
2016-03-18 14:36:58 -0300

    TimeBox version 0.10.7:
    - Fixes an issue with TB.SHARPSHOOT, implementing options similar to
      those of TB.SCAT.
    - Fixes an issue with TB.SCAT which caused arguments to be improperly
      parsed and sometimes ignored.
    - Removes deprecated field RELEASEDATE of release version as returned by
      TB.VERSION.

--- +tb/version.m
+++ +tb/version.m
@@ -12,7 +12,7 @@ function ver = version
 ver = struct;
 ver.major = 0;                               % major version: increase with "big" changes
 ver.minor = 10;                              % minor version: increase with bug fixes and "small" new functionalities
-ver.release = 6;                             % release number: increase at every release
+ver.release = 7;                             % release number: increase at every release
 ver.releasedatenum = datenum(2016, 03, 18);  % date number value for the date of release
 ver.special = '';                            % set flags for particular versions
 end
