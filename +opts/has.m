8e558966fbc5a81c1bb0bf39fc9bd71c75e71367
Rafael Giusti <rfgiusti@gmail.com>
2016-04-28 11:30:55 -0300

    Adde revision information for the OPTS package

--- +opts/has.m
+++ +opts/has.m
@@ -2,4 +2,7 @@ function out = has(option, key)
 %OPTS.HAS Check for existence of key in an OPTS object.
 %   OPTS.HAS(options, k) returns 1 if "options" is an OPTS object and it
 %   contains a vlaue for the key "k"; returns 0 otherwise.
+
+%   This file is part of TimeBox. Copyright 2015-16 Rafael Giusti
+%   Revision 1.0
 out = opts.isa(option) && option.isKey(key);
