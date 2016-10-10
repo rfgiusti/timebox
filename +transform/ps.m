cef3c4d312e0d905be5c680062ce91df43a34751
Rafael Giusti <rfgiusti@gmail.com>
2016-10-10 15:07:33 -0300

    Deprecates TRANSFORM.PS
    - The function TRANSFORM.PS will be removed in favor of TRANSFORM.FS
    and TRANSFORM.PSD

--- +transform/ps.m
+++ +transform/ps.m
@@ -14,9 +14,14 @@ function [train_ps, test_ps] = ps(train, test, ~)
 %   Class values are expected in the first column of the dataset.
 %
 %   This function takes no optional arguments.
+%
+%   This function has been deprecated since TimeBox 0.12 and will be
+%   removed in a future release.
 
 %   This file is part of TimeBox. Copyright 2015-16 Rafael Giusti
-%   Revision 0.1
+%   Revision 0.2.0
+warnobsolete('transform:ps');
+
 train_ps = [train(:, 1), abs(fft(train(:, 2:end), [], 2))];
 if exist('test', 'var')
     test_ps = [test(:, 1), abs(fft(test(:, 2:end), [], 2))];
