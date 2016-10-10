2ec84ff1873eecd8618cc510b4dc95054aef3549
Rafael Giusti <rfgiusti@gmail.com>
2016-10-10 14:08:33 -0300

    Add usage example to TRANSFORM.AMP

--- +transform/amp.m
+++ +transform/amp.m
@@ -10,9 +10,24 @@ function [train, test] = amp(train, test, ~)
 %   Class values are expected in the first column of the dataset.
 %
 %   This function takes no optional arguments.
+%
+%   Example:
+%
+%       % In timebox/assets/wav there is a WAVE file containing 1 second of
+%       % the note "middle-C" (C4). The following example reads that file,
+%       % makes an artificial data set containing a single example and uses
+%       % TRANSFORM.AMP to generate an off-scale 1-sided amplitude
+%       % spectrum. There will be a peak at approximately 261.6 Hz
+%       [y, Fs] = wavread('assets/wav/c4.wav');
+%       ds = [1, y(:)'];
+%       Y = transform.amp(ds);
+%       Y(1) = [];
+%       Y = 10 * log10(Y(1:end/2 + 1));
+%       freq = 0:Fs/numel(y):Fs/2;
+%       plot(freq, Y);
 
 %   This file is part of TimeBox. Copyright 2015-16 Rafael Giusti
-%   Revision 1.0.0
+%   Revision 1.0.1
 train = [train(:, 1), abs(fft(train(:, 2:end), [], 2))];
 if exist('test', 'var')
     test = [test(:, 1), abs(fft(test(:, 2:end), [], 2))];
