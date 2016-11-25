04d518d3342247d512427857ae2d76652af751a0
Rafael Giusti <rfgiusti@gmail.com>
2016-11-25 15:52:11 -0200

    TimeBox v0.12
    - TRANSFORM.SAX no longer normalizes the input.
    - TRANSFORM.SAX is set to use a standard normal to convert PAA segments
      into SAX letters. The option "sax::cut function" was removed and the
      functions TRANSFORM.SAX.BELL and TRANSFORM.SAX.UNIF were removed as
      well.
    - TRANSFORM.PAA no longer forces pieces to be extracted from integral
      observations. The default behavior is to partially weigh observations
      in accordance to the original proposal. It is possible to invoke the
      previous behavior by setting "paa::constant" to 0.
    - TRANSFORM.PAA verifies if number of segments and segment length are
      appropriate to the input series.
    - TRANSFORM.FS was introduced to find the frequency/amplitude spectrum
      of the time series.
    - TRANSFORM.PSD calculates the power spectral density of the series.
    - Deprecated TRANSFORM.PS.
    - Introduced DITS.MINDIST and DISTS.MDLOOKUP to calculate the distance
      between SAX words more closely to the paper design. DIST.MINDIST does
      not require knowledge of the original length of the time series and,
      therefore, is only proportional to the original MINDIST by a factor
      that depends on the length of the series and of the sax word.
    - TRANSFORM.SAX has been redesigned to work much faster.
    - TRANSFORM.ACF is now a proxy to Matlab's AUTOCORR, which runs much
      faster than the previous implementation.
    - Added WAVE files with simple sinusoidal waves in "./assets/".

--- +tb/version.m
+++ +tb/version.m
@@ -2,23 +2,24 @@ function ver = version
 %TB.VERSION     Return the version of TimeBox
 %   Release dates:
 %
-%   0.11.9 - May-31-2016
-%   0.11.8 - Apr-21-2016
-%   0.10.7 - Mar-18-2016
-%   0.10.6 - Mar-04-2016
-%   0.9.5  - Nov-30-2015
-%   0.8.4  - Sep-14-2015
-%   0.8.3  - Sep-09-2015
-%   0.7.2  - May-19-2015
-%   0.6.1  - Apr-27-2015
+%   0.12    - Nov-25-2016
+%   0.11.9  - May-31-2016
+%   0.11.8  - Apr-21-2016
+%   0.10.7  - Mar-18-2016
+%   0.10.6  - Mar-04-2016
+%   0.9.5   - Nov-30-2015
+%   0.8.4   - Sep-14-2015
+%   0.8.3   - Sep-09-2015
+%   0.7.2   - May-19-2015
+%   0.6.1   - Apr-27-2015
 
 %   This file is part of TimeBox. Copyright 2015-16 Rafael Giusti
 %   Revision 1.1.0
 ver = struct;
 ver.major = 0;                               % major version
-ver.minor = 11;                              % minor version
-ver.patch = 9;                               % patch version
-ver.release = 9;                             % release number: deprecated since v0.11.9
-ver.releasedatenum = datenum(2016, 05, 31);  % date number value for the date of the last release
+ver.minor = 12;                              % minor version
+ver.patch = 0;                               % patch version
+ver.release = 0;                             % release number: deprecated since v0.11.9
+ver.releasedatenum = datenum(2016, 11, 07);  % date number value for the date of the last release
 ver.special = '';                            % set flags for particular versions
 end
