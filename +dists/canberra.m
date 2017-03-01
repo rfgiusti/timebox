ddf2d6fbca654f8e44f93daa6484b02e26767d8b
Rafael Giusti <rfgiusti@gmail.com>
2017-03-01 18:43:02 -0300

    Canberra distance: considers 0/0 = 0

--- +dists/canberra.m
+++ +dists/canberra.m
@@ -1,8 +1,13 @@
 function d = canberra(P, Q)
 %DISTS.   distance between two time series
-%   (S,Z) returns the distance between time series S and Z
+%   (S,Z) returns the Canberra distance between time series S and Z.
+%   Considers 0/0 = 0 and employs the absolute of the sum in the
+%   denominator.
 
 %   This file is part of TimeBox. Copyright 2015-16 Rafael Giusti
-%   Revision 0.1
-d = sum(abs(P - Q) ./ (P + Q));
+%   Revision 1.0.0
+PQdiff = abs(P - Q);
+PQsum = abs(P + Q);
+PQsum(PQsum < eps) = 1;
+d = sum(PQdiff ./ PQsum);
 end
