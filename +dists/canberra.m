b43e122b9603c214778e3a88a8fc14b189b4f2c8
Rafael Giusti <rfgiusti@gmail.com>
2017-03-10 20:29:56 -0300

    Fix Canberra distance
    - The Canberra denominator was incorrectly implemented as |S+Z|, where
    |.| is the absolute value; it's not fixed to |S|+|Z|

--- +dists/canberra.m
+++ +dists/canberra.m
@@ -4,10 +4,10 @@ function d = canberra(P, Q)
 %   Considers 0/0 = 0 and employs the absolute of the sum in the
 %   denominator.
 
-%   This file is part of TimeBox. Copyright 2015-16 Rafael Giusti
-%   Revision 1.0.0
+%   This file is part of TimeBox. Copyright 2015-17 Rafael Giusti
+%   Revision 2.0.0
 PQdiff = abs(P - Q);
-PQsum = abs(P + Q);
+PQsum = abs(P) + abs(Q);
 PQsum(PQsum < eps) = 1;
 d = sum(PQdiff ./ PQsum);
 end
